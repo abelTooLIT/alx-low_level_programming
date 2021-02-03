92a5d543392ad1bff4c9e6c864d67c20ccb8a2cf
Abel Fisseha <51496964+abelTooLIT@users.noreply.github.com>
2021-02-03 22:18:46 +0300

    print with f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - first line to compile
+ *
+ * Returns: Always 0 
+ */
 int main(void)
 {
   printf("with proper grammar, but, the outcome is a piece of art,\n");
