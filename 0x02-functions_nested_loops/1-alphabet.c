a814262dd6a3b22322ead42b57413f70147a1e17
Abel Fisseha <51496964+abelTooLIT@users.noreply.github.com>
2021-02-07 18:29:59 -1200

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+  * print_alphabet - alphabet the code for Holberton School students.
+  * @void: no content
+  *
+  */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+
+	}
+		_putchar('\n');
+}
