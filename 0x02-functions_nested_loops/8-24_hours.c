050ba87d42119eded20bbea6cdba5813daecd4ae
Abel Fisseha <51496964+abelTooLIT@users.noreply.github.com>
2021-02-07 20:28:11 -1200

    24hr every min

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+ void jack_bauer(void)
+{
+int i;
+	int j;
+	char del = ':';
+
+	for (i = 00; i <= 23; i++)
+	{
+		for (j = 00 ; j <= 59; j++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(del);
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
