a8ff2426ed4020b2f53f802e6490fc0832cf5726
Abel Fisseha <51496964+abelTooLIT@users.noreply.github.com>
2021-02-09 12:35:46 +0300

    First multiple main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,14 +10,12 @@ int main(void)
         int i;
 
         printf("Infinite loop incoming :(\n");
-
  /*
  * while (i < 10)
  *{
  *putchar(i);
  *}
  */
-
         printf("Infinite loop avoided! \\o/\n");
 
         return (0);
