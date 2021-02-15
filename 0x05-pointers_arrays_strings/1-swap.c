50298dacbb443f97c078a6688a12965b4d030e61
Abel Fisseha <51496964+abelTooLIT@users.noreply.github.com>
2021-02-16 01:15:25 +0300

    swaps the values of two integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,29 @@
 #include "holberton.h"
+<<<<<<< HEAD
+
 /**
- * swap_int - function to reset n to 98
- * @a: first integer
- * @b: second integer
- * void: no content
+ * swap_int - swap values
+ * @a: first int
+ * @b: second int
+ */
+
+void swap_int(int *a, int *b)
+{
+int c;
+
+c = *a;
+*a = *b;
+*b = c;
+=======
+/**
+ * swap_int - check the code for Holberton School students.
+ * @a: is a pointer
+ * @b: is a pointer
  */
 void swap_int(int *a, int *b)
 {
-	int tmp;
-
-	tmp = *b;
-	*b = *a;
-	*a = tmp;
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+>>>>>>> 903de5e76a00e0adf8218b929c786851db4b3957
 }
