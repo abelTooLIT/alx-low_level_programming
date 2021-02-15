b68d141bcc437ef1d56352fbca222e824536e644
Abel Fisseha <51496964+abelTooLIT@users.noreply.github.com>
2021-02-16 01:11:06 +0300

    takes a pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,24 @@
 #include "holberton.h"
+<<<<<<< HEAD
+
 /**
- * reset_to_98 - function to reset n to 98
- * @n: pointing
- * void: no content
+ * reset_to_98 - reset int 98
+ * @n: int to reset
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+
+*n = 98;
+=======
+/**
+ * reset_to_98 - change value of *n to 98
+ * @n: is a pointer
  */
 void reset_to_98(int *n)
 {
-	*n = 98;
+	int p = 98;
+	*n = p;
+>>>>>>> 903de5e76a00e0adf8218b929c786851db4b3957
 }
