00f794bd0536fb889b9f809128272f54659bd64c
Abel Fisseha <51496964+abelTooLIT@users.noreply.github.com>
2021-02-16 20:11:52 +0300

    point 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -11,4 +11,14 @@ void reset_to_98(int *n)
 {
 
 *n = 98;
+=======
+/**
+ * reset_to_98 - change value of *n to 98
+ * @n: is a pointer
+ */
+void reset_to_98(int *n)
+{
+	int p = 98;
+	*n = p;
+>>>>>>> 903de5e76a00e0adf8218b929c786851db4b3957
 }
