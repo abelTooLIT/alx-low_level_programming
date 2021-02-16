9be09faf0063c706c4543014dc9c36c5af306d5d
Abel Fisseha <51496964+abelTooLIT@users.noreply.github.com>
2021-02-16 21:10:21 +0300

    convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,51 +1,35 @@
 #include "holberton.h"
-#include <limits.h>
-
+#include <stdio.h>
 /**
- * _atoi - Convert a string to an integer
- * @s: Pointer to the string to convert
+ * _atoi - fun to convert string to integer
+ * @s: string
+ * Return: no zero
  *
- * Return: The integer that was converted
  */
 int _atoi(char *s)
 {
-int i, start, negative;
-unsigned int number;
+	int sign = 0;
+	unsigned int num = 0;
 
-start = -1;
-negative = 0;
-number = 0;
-for (i = 0; s[i] != '\0'; i++)
-{
-if (s[i] >= '0' && s[i] <= '9')
-start = i;
-
-if (s[i] == '-' && start == -1)
-{
-if (negative)
-negative = 0;
-else
-negative = 1;
-}
-}
-
-if (start != -1)
-{
-for (i = 0; s[i] != '\0'; i++)
-{
-if (s[i] >= '0' && s[i] <= '9')
-number = number * 10 + s[i] - '0';
-else if (s[i - 1] >= '0' && s[i - 1] <= '9')
-break;
-}
-}
-
-if ((number >= (unsigned int) INT_MAX && negative == 0))
-return (INT_MAX);
-else if ((number > (unsigned int) INT_MAX && negative))
-return (INT_MIN);
-else if (negative)
-return ((int) -number);
-else
-return ((int) number);
+	while (*s != '\0')
+	{
+		if (*s == '-')
+		{
+			sign++;
+		}
+		if (*s >= '0' && *s <= '9')
+		{
+		num = (num * 10) + (*s - '0');
+		}
+		if (*s == ';')
+		{
+			break;
+		}
+		s++;
+	}
+	if (sign % 2 != 0)
+	{
+		return (num * (-1));
+	}
+		return (num);
 }
