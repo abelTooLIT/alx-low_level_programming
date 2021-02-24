8a3b0781e7a443724a73eb86ebd705a25d8a1851
Abel Fisseha <51496964+abelTooLIT@users.noreply.github.com>
2021-02-24 15:17:48 +0300

    power of

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,31 @@
 #include "holberton.h"
 /**
-* raiz -  function to find a square
-* @n: number
-* @a: counter
-* Return: no zero
-*/
-int raiz(int n, int a)
+  * _sqrt_recursion - returns the natural square root of a number
+  * @n: takes in an integer
+  * Return: a starting number and n to compare
+  */
+int _sqrt_recursion(int n)
 {
-	if (a * a > n)
+	return (helper_julien(1, n));
+}
+/**
+  * helper_julien - helps to find square root
+  * @x: takes in an integer
+  * @y: takes in an integer
+  * Return: return x compared to y
+  */
+int helper_julien(int x, int y)
+{
+	if (x * x == y)
+	{
+		return (x);
+	}
+	else if (x * x > y)
 	{
 		return (-1);
 	}
-	else if (a * a == n)
-	{
-		return (a);
-	}
 	else
 	{
-		return (raiz(n, a + 1));
+	return (helper_julien(x += 1, y));
 	}
 }
