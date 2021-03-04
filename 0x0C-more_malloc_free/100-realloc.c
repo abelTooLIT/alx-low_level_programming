2641119b144128f9c80d0403b267597ea8646613
Abel Fisseha <51496964+abelTooLIT@users.noreply.github.com>
2021-03-04 22:40:55 +0300

    adv

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,30 +1,33 @@
-#include <stdlib.h>
 #include "holberton.h"
+
 /**
-* _realloc - function to reallocate memory using malloc
-* @ptr: array
-* @old_size: old size of array
-* @new_size: new size to reallocate
-* Return: ptr
-*/
+ * _realloc -  reallocates a memory block using malloc and free
+ * @ptr: pointer
+ * @old_size: old size
+ * @new_size: new size
+ * Return: pointer
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	char *clone, *relloc;
+	unsigned int i;
 
-	ptr = (void *) malloc(old_size);
-	if (ptr == NULL && old_size == new_size)
-	{
-		return (NULL);
-	}
+	if (ptr != NULL)
+	clone = ptr;
+	else
+	{ return (malloc(new_size)); }
+	if (new_size == old_size)
 	return (ptr);
-	if (ptr == NULL || new_size > old_size)
-	{
-		free(ptr);
-	}
-	ptr = (void *) malloc(new_size);
 	if (new_size == 0 && ptr != NULL)
+	{ free(ptr);
+	return (0); }
+	relloc = malloc(new_size);
+	if (relloc == NULL)
+	return (0);
+	for (i = 0; i < (old_size || i < new_size); i++)
 	{
-		free(ptr);
-		return (NULL);
+		*(relloc + i) = clone[i];
 	}
-		return (ptr);
+	free(ptr);
+return (relloc);
 }
