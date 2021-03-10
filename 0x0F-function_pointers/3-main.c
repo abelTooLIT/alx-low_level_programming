d8a1c90c287d6d5c558c14cd403c87440562b707
Abel Fisseha <51496964+abelTooLIT@users.noreply.github.com>
2021-03-10 22:48:43 +0300

    main only

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,37 +1,31 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * main - function argc and argv
- * @argc: number of arguments
- * @argv: vector or punters of arrays
- * Return: always zero
+ * main - Entry point
+ * @argc: Num of arguments
+ * @argv: Array of arguments
+ * Return: 0 on success
  */
+
 int main(int argc, char *argv[])
 {
-	int a, b, c;
-	int (*operation)(int, int);
-
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	operation = get_op_func(argv[2]);
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-
-	if (operation == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	if (((operation == op_div) || (operation == op_mod)) && (b == 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	c = operation(a, b);
-	printf("%d\n", c);
-	return (0);
+if (argc != 4)
+{
+printf("Error\n");
+return (98);
+}
+if (!get_op_func(argv[2]) || argv[2][1] != '\0')
+{
+printf("Error\n");
+return (99);
+}
+if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
+{
+printf("Error\n");
+return (100);
+}
+printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+return (0);
 }
