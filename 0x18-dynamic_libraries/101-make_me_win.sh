3aab318bec93c59e6e671a4897cc90508482943f
Abel Fisseha <51496964+abelTooLIT@users.noreply.github.com>
2021-05-03 23:52:29 +0300

    .

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone https://github.com/alx-low_level_programming.git ~
-LD_PRELOAD=~/alx-low_level_programming/0x18-dynamic_libaries/crack.so
+wget -P /tmp/ https://raw.github.com/abelTooLIT/alx-low_level_programming/master/0x18-dynamic_libraries/librich.so
+export LD_PRELOAD=/tmp/librich.so
